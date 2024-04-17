70d22448e77321b395781bd23801ce00ecfb98ba
Erika <brandonerika96@gmail.com>
2024-04-17 19:36:24 -0400

    askGuess function done

--- guessing-game.js
+++ guessing-game.js
@@ -1,26 +1,43 @@
+// import the readline module into our file
+const readline = require("readline");
+
+// create an interface where we can talk to the user
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+  });
+
 const secretNumber = 12;    // Later we will program this variable to be assigned at random
 
 function checkGuess(num) {
 
-    if(num > secretNumber) {
+    if(num > Number(secretNumber)) {
         console.log('Too high.');
         return false;
     }
 
-    if(num < secretNumber) {
+    if(num < Number(secretNumber)) {
         console.log('Too low.');
         return false;
     }
 
-    if(num === secretNumber) {
-        console.log('Correct!');
+    if(num == Number(secretNumber)) {
+        console.log('You win!');
         return true;
     }
 }
 
-// console.log(checkGuess(7));
-// console.log(checkGuess(1));
-// console.log(checkGuess(-4));
-// console.log(checkGuess(17));
-// console.log(checkGuess(543757));
-// console.log(checkGuess(12));
+function askGuess() {
+    rl.question('Enter a guess ', (answer) => {
+        checkGuess(answer); //invoke checkGuess function
+        if (answer != Number(secretNumber)) {
+            askGuess();
+        }
+        if (answer == Number(secretNumber)) {
+            rl.close(); //closes interface
+        }
+      });
+}
+
+
+askGuess();
