9dc8ee3ae81b67e23007536233dda5f33a9dac54
robweekes <weekes.robert@gmail.com>
2024-04-18 16:21:20 -0700

    finished askRange func and added Number(num1) to randomInRange to force user input into # format

--- guessing-game.js
+++ guessing-game.js
@@ -9,16 +9,23 @@ const rl = readline.createInterface({
 
 // random number generator, from num1 to num2
 let randomInRange = function(num1, num2) {
+    num1 = Number(num1);
+    num2 = Number(num2);
     let range = num2 - num1;
-    // console.log(range);
-    let random = num1 + Math.round(range * Math.random());
-    // console.log(random);
-    return random;
+    console.log(num1);
+    console.log(num2);
+    console.log(range);
+    let randNum = Math.random();
+    console.log(randNum);   // decimal between 0 and 1
+    let random = num1 + Math.round(range * randNum);
+    console.log(typeof random);
+    let secretNumber = random;
+    console.log(secretNumber); // integer in user specified range
+    return secretNumber;
 }
-// randomInRange(15, 20);   // generates #'s from 15-20 incl.
+// randomInRange(28, 68); // generates #'s from x to y incl. (for testing num generator)
 
-
-let secretNumber = randomInRange(0, 100);
+// let secretNumber = randomInRange(0, 100);  // testing hard #'s
 // console.log(secretNumber);
 // const secretNumber = 12;   // testing hard #
 
@@ -58,16 +65,21 @@ function askGuess() {
 function askRange() {
     //asking user for min
     rl.question('Enter a min number: ', (minNum) => {
-        if (typeof minNum == "number") {
-            console.log(minNum);
+        // if (typeof minNum == "number") {
+        if (minNum.length > 0) {
         }
 
         rl.question('Enter a max number: ', (maxNum) => {
-            if (typeof maxNum == "number") {
+            // if (typeof maxNum == "number") {
+            if (maxNum.length > 0) {
+                rl.close(); //closes interface
+                console.log(`I'm thinking of a number between ${minNum} and ${maxNum}...`);
+                console.log(minNum);
                 console.log(maxNum);
+                randomInRange(minNum, maxNum);
             }
         });
     });
 }
 
-askRange()
+askRange();
